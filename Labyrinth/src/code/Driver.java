ea08e3467cd967df9ee2322da648cef087c60d10
Himayet <Himayet@Himayet-PC>
2016-04-04 19:06:34 -0400

    Updates in GUI

--- Labyrinth/src/code/Driver.java
+++ Labyrinth/src/code/Driver.java
@@ -4,11 +4,13 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import code.gui.GUI;
+import code.gui.GUI;
 import code.model.LabyrinthModel;
 
 public class Driver {
       
     public static void main(String[] args){
+    	SwingUtilities.invokeLater(new GUI(new LabyrinthModel()));
     }
 }
  
--- Labyrinth/src/code/gui/GUI.java
+++ Labyrinth/src/code/gui/GUI.java
@@ -1,5 +1,10 @@
 package code.gui;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,7 +15,7 @@ import code.model.Observer;
 
 public class GUI implements Runnable,Observer {
 
-	private JFrame _jf;
+	private JFrame _window;
 	private JPanel _jp;
 	private LabyrinthModel _lm;
 	private Tile _tile;
@@ -20,11 +25,24 @@ public class GUI implements Runnable,Observer {
 		_lm = lm;
 		_lm.setObserver(this);
 	}
+	
 	@Override public void run() {
-	
-	
-	
+		_window = new JFrame("Labyrinth Team 125 Edition");
+		_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		_window.getContentPane().setLayout(new FlowLayout());
+		_window.setFocusable(true);
+//		_window.setFocusTraversalKeysEnabled(false);
+		
+		_jp = new JPanel();
+		_jp.setFocusable(true);
+		_jp.setLayout(new GridLayout(LabyrinthModel.ROWS, LabyrinthModel.COLS));
+		
+		
+		_window.pack();
+		_window.setVisible(true);
+		
 	}
+	
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
