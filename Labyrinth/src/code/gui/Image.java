2d5c443c136b7773833f26bf4e0205ccddcaaaa5
AdityaKishan <AdityaKishan@AdityaKishan>
2016-04-06 20:08:20 -0400

    Just started the Image class

--- Labyrinth/src/code/gui/Image.java
+++ Labyrinth/src/code/gui/Image.java
@@ -1,5 +1,11 @@
 package code.gui;
 
-public class Image {
+import java.awt.image.BufferedImage;
 
+public class Image {
+       private BufferedImage img;
+       
+       public Image(){
+    	   img=Image.IO.read(new File())
+       }
 }
